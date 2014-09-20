45f17cd90cb9bdbb9f0c48a167f514bdc02d8a19
Yuki KAN <re@pixely.jp>
2014-09-21 01:33:47 +0900

    Initial Commit

---
+++ Yabumi/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Yabumi
+//
+//  Created by Yuki KAN on 2014/09/21.
+//  Copyright (c) 2014年 Webnium Inc. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
